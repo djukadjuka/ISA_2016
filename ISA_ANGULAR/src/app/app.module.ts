e52bfed2c601fdbc0b89b79ae69e011e214c53ce
Stevan Kosijer <stkosijer@gmail.com>
2016-12-20 02:01:21 +0100

    Routing of the application. Bootstraped the application. Added the Navbar and NotFound components.

--- ISA_ANGULAR/src/app/app.module.ts
+++ ISA_ANGULAR/src/app/app.module.ts
@@ -1,24 +1,36 @@
+//Module imports 
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router'
+import { routing } from './app.routing'
+
+//Component imports
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
+//Service imports
+
 import { TestService } from './test/test.service';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule
+    RouterModule,
+    routing
   ],
   providers: [
     TestService
