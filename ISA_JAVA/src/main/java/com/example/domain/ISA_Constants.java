8682770005130f627587ba3fd4a6db5a51e8ea3e
djukadjuka <milanns@live.com>
2017-02-03 17:36:32 +0100

    Restaurant Editing[0]
    Added a Restaurant type Table that contains possible restaurant types. A restaurant can be more than one type (?), so I had to add this and connect it to the restaurant via relation. Now a checkbox or pick list can be implemented in editing a restaurant (Choosing which type(s) a restaurant is). This can also be implemented for which types of food a restaurant serves, or not, if we're going by menu.

---
+++ ISA_JAVA/src/main/java/com/example/domain/ISA_Constants.java
@@ -0,0 +1,22 @@
+package com.example.domain;
+
+public class ISA_Constants {
+
+	//RESTAURANT TYPES
+	public static final String RESTAURANT_TYPE_FINE_DINING = "Fine Dining";
+	public static final String RESTAURANT_TYPE_FAST_FOOD = "Fast Food";
+	public static final String RESTAURANT_TYPE_BISTRO = "Bistro";
+	public static final String RESTAURANT_TYPE_SPORTS = "Sports Bar";
+	public static final String RESTAURANT_TYPE_DINER = "Diner";
+	
+	//FOOD TYPES
+	public static final String FOOD_TYPE_CHINESE = "Chinese Cuisine";
+	public static final String FOOD_TYPE_SERBIAN = "Serbian Cuisine";
+	public static final String FOOD_TYPE_ITALIAN = "Italian Cuisine";
+	public static final String FOOD_TYPE_SPANISH = "Spanish Cuisine";
+	public static final String FOOD_TYPE_MEXICAN = "Mexican Cuisine";
+	public static final String FOOD_TYPE_AMERICAN = "American Cuisine";
+	public static final String FOOD_TYPE_INDIAN = "Indian Cuisine";
+	public static final String FOOD_TYPE_JAMAICAN = "Jamaican Cuisine";
+	
+}
---
+++ ISA_JAVA/src/main/java/com/example/domain/RestaurantTypeBean.java
@@ -0,0 +1,13 @@
+package com.example.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class RestaurantTypeBean {
+
+	private long id;
+	private String name;
+	
+	
+	
+}
