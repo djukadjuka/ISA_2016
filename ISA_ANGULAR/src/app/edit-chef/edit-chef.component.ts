88cd94711e3755b046540729c098abfe85594c1a
StefanTen <stefanjanjiczvornik@gmail.com>
2017-02-21 00:23:16 +0100

    Schedule for employees

--- ISA_ANGULAR/src/app/edit-chef/edit-chef.component.ts
+++ ISA_ANGULAR/src/app/edit-chef/edit-chef.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from 'primeng/primeng';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EditUserService } from '../edit-user/edit-user.service';
+import { SharedService } from '../shared/shared.service';
+import {ViewRestaurantsService} from '../restaurants/view-restaurants/view-restaurants.service';
 
 @Component({
   selector: 'app-edit-chef',
@@ -9,14 +14,77 @@ export class EditChefComponent implements OnInit {
 
 
   private displayScheduleButton: boolean =false;
-  constructor() { }
+   private user = {};
+    private userUpdate = {username: "", id: ""};
+    private msgs: Message[] = [];
+    private formEditUser: FormGroup;
+  constructor(
+
+     private _editUserService: EditUserService,
+     private _restaurantService : ViewRestaurantsService,
+    
+    private _fb: FormBuilder,
+    private _sharedService : SharedService,
+  ) { }
 
   ngOnInit() {
+
+       this.formEditUser = this._fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required]
+    });
+    
+
+
+
+    this._editUserService.getUserById(this._sharedService.userId)
+      .subscribe(
+      res => this.userUpdate = res
+      );
+
+    console.log(this.userUpdate);
+/*
+     this._restaurantService.getWorkersNOMANAGERSForRestaurant(1).subscribe(
+          res=>this.userUpdate = res
+      );
+
+    console.log(this.userUpdate);
+    */
   }
+
+
+
+  
 
 
   displaySchedule(){
     this.displayScheduleButton = true;
 
   }
+
+  
+    updateUser()
+  {
+      this._editUserService.checkUsername(this.userUpdate.username, this.userUpdate.id)
+                            .subscribe(
+                              res => 
+                              {
+                                if(res) {
+                                  this.msgs = [];
+                                  this.msgs.push({severity:'error', summary:'That username is taken!', detail:'Choose another username.'});
+                                  }
+                                else
+                                  this._editUserService.updateUser(this.userUpdate)
+                                                        .subscribe(
+                                                                 res => 
+                                                                  {
+                                                                  
+                                                                    this.user = this.userUpdate;
+                                                                  }
+                                                                );  
+                                }
+                            )           
+  }
+
 }
--- ISA_ANGULAR/src/app/edit-waiter/edit-waiter.component.ts
+++ ISA_ANGULAR/src/app/edit-waiter/edit-waiter.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TablesClass } from '../tables/tables-class';
 import { TablesService } from '../tables/tables.service';
+import { Message } from 'primeng/primeng';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EditUserService } from '../edit-user/edit-user.service';
+import { SharedService } from '../shared/shared.service';
 
 @Component({
   selector: 'app-edit-waiter',
@@ -12,15 +16,38 @@ export class EditWaiterComponent implements OnInit {
 
    private displayScheduleButton: boolean = false;
    private showTablesButton: boolean = false ;
+   private user = {};
+    private userUpdate = {username: "", id: ""};
+    private msgs: Message[] = [];
+    private formEditUser: FormGroup;
 
    tables: TablesClass[];
 
   constructor(
- private tablesService: TablesService
+ private _editUserService: EditUserService,
+    private tablesService: TablesService,
+    private _fb: FormBuilder,
+    private _sharedService : SharedService,
 
   ) { }
 
   ngOnInit() {
+
+       this.formEditUser = this._fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required]
+    });
+    
+
+
+
+    this._editUserService.getUserById(this._sharedService.userId)
+      .subscribe(
+      res => this.userUpdate = res
+      );
+
+    console.log(this.userUpdate);
     
   }
 
@@ -39,6 +66,29 @@ export class EditWaiterComponent implements OnInit {
 
     this.showTablesButton = true;
 
+  }
+
+    updateUser()
+  {
+      this._editUserService.checkUsername(this.userUpdate.username, this.userUpdate.id)
+                            .subscribe(
+                              res => 
+                              {
+                                if(res) {
+                                  this.msgs = [];
+                                  this.msgs.push({severity:'error', summary:'That username is taken!', detail:'Choose another username.'});
+                                  }
+                                else
+                                  this._editUserService.updateUser(this.userUpdate)
+                                                        .subscribe(
+                                                                 res => 
+                                                                  {
+                                                                  
+                                                                    this.user = this.userUpdate;
+                                                                  }
+                                                                );  
+                                }
+                            )           
   }
 
 }
