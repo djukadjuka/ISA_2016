ee48fc332f8ffe7af695d3176eabd4d2c0fe396b
StefanTen <stefanjanjiczvornik@gmail.com>
2017-02-28 00:00:43 +0100

    Creating order[0]

--- ISA_JAVA/src/main/java/com/example/service/OrderService.java
+++ ISA_JAVA/src/main/java/com/example/service/OrderService.java
@@ -1,5 +1,13 @@
 package com.example.service;
 
+import java.util.Collection;
+
+import com.example.domain.OrderBean;
+
 public interface OrderService {
 
+	
+	
+	public Collection<OrderBean> updateOrder(float price,String name ,Long table,Long waiter );
+	
 }
